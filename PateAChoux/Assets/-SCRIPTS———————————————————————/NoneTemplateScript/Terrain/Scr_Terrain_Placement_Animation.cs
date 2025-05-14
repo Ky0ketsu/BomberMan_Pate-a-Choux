242c8cac7fa3d602b4056f052d393c5e6f4fc293
JORDAN <>
2025-05-14 16:50:10 +0200

    Script bloc destructible apparition

--- PateAChoux/Assets/-SCRIPTS———————————————————————/NoneTemplateScript/Terrain/Scr_Terrain_Placement_Animation.cs
+++ PateAChoux/Assets/-SCRIPTS———————————————————————/NoneTemplateScript/Terrain/Scr_Terrain_Placement_Animation.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class Scr_Terrain_Placement_Animation : MonoBehaviour
 {
     [SerializeField] private float timerBeforePlacement;
-    Transform graphics => transform.Find("Graphics");
+    Transform graphics => transform.Find("Graphics"); 
+    AudioSource BlocApparitionAudioSource;
 
     private void Start()
     {
         graphics.localScale = Vector3.zero;
         timerBeforePlacement = (transform.localPosition.x + transform.localPosition.z) / 20 + 1.5f;
         StartCoroutine(AnimationPlacement());
+        BlocApparitionAudioSource = GetComponent<AudioSource>();
     }
 
     IEnumerator AnimationPlacement()
@@ -20,6 +22,7 @@ public class Scr_Terrain_Placement_Animation : MonoBehaviour
         yield return new WaitForSeconds(timerBeforePlacement);
         graphics.DOKill();
         graphics.DOScale(Vector3.one, 2).SetEase(Ease.OutBounce).From(0);
+        BlocApparitionAudioSource.Play();
     }
 
 
