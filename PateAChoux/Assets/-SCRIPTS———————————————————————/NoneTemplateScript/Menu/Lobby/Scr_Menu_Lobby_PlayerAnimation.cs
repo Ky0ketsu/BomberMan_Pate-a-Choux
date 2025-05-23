7a34a47dd1ff48dd6d1bfd0456774a193f198462
Ky0h0 <daymondeschamps@gmail.com>
2025-05-23 02:50:36 +0200

    lobby ready animation

--- PateAChoux/Assets/-SCRIPTS———————————————————————/NoneTemplateScript/Menu/Lobby/Scr_Menu_Lobby_PlayerAnimation.cs
+++ PateAChoux/Assets/-SCRIPTS———————————————————————/NoneTemplateScript/Menu/Lobby/Scr_Menu_Lobby_PlayerAnimation.cs
@@ -7,6 +7,7 @@ public class Scr_Menu_Lobby_PlayerAnimation : MonoBehaviour
 {
     public GameObject player;
     public GameObject controller;
+    public GameObject ready;
     public float time;
 
     public void Start()
@@ -18,6 +19,7 @@ public class Scr_Menu_Lobby_PlayerAnimation : MonoBehaviour
     public void AnimationSpawn()
     {
         controller.transform.DOScale(Vector3.zero, time).SetEase(Ease.InBounce);
+        ready.transform.DOScale(Vector3.one, time).SetEase(Ease.OutBounce);
         player.transform.DOScale(Vector3.one, time).SetEase(Ease.OutBounce).OnComplete(StartYoyo);
     }
 
@@ -31,5 +33,6 @@ public class Scr_Menu_Lobby_PlayerAnimation : MonoBehaviour
         player.transform.DOKill();
         controller.transform.DOScale(Vector3.one, 0.1f);
         player.transform.DOScale(Vector3.zero, 0.1f);
+        ready.transform.DOScale(Vector3.zero, 0.1f);
     }
 }
